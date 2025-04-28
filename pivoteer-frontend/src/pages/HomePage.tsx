dd3483d4b9af5f0a6d4cace59f2298857ce96f64
ekrusznis <ekrusznis@gmail.com>
2025-04-28 09:14:58 -0400

    updated landing page sections - ek

--- pivoteer-frontend/src/pages/HomePage.tsx
+++ pivoteer-frontend/src/pages/HomePage.tsx
@@ -1,24 +1,39 @@
 import Service from "@/components/custom_components/HomePage/Service";
 import { pivoteerServices } from "@/components/custom_components/HomePage/services";
+import Features from "@/components/custom_components/HomePage/Features";
+import Pricing from "@/components/custom_components/HomePage/Pricing";
+import ContactForm from "@/components/custom_components/HomePage/ContactForm";
 import { Button } from "@/components/ui/button";
 
 const HomePage = () => {
   return (
     <div className="flex flex-col justify-center items-center">
       <div className="flex flex-col justify-center items-center gap-3 mt-10">
-        <div className="w-[200px] h-[200px] bg-electricPurple flex justify-center items-center rounded-full">
-          Logo placeholder
-        </div>
+        <img
+          src="logo_500.png"
+          alt="My Pivoteer Logo"
+          className="w-[200px] h-[200px] rounded-full object-cover"
+        />
         <p className="font-semibold text-5xl">MY PIVOTEER</p>
-        <p>Tranform Your Spreadsheets with Ease</p>
+        <p>Transform Your Spreadsheets with Ease</p>
         <Button size="lg">Get started</Button>
       </div>
 
+      {/* Services */}
       <div className="grid grid-cols-3 gap-5 w-[70%] mt-10">
         {pivoteerServices.map((service) => (
           <Service key={service.id} pivoteerService={service} />
         ))}
       </div>
+
+      {/* Features */}
+      <Features />
+
+      {/* Pricing */}
+      <Pricing />
+
+      {/* Contact Us */}
+      <ContactForm />
     </div>
   );
 };
